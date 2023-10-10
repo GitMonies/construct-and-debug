c7992f2957d4e2f0a3fd4019e513052cf352c11a
GitMonies <jarod.smelosky@yahoo.com>
2023-10-09 20:25:51 -0500

    Creating a recipe Model & adding content to recipes components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+recipes: Recipe[] = [
+  new Recipe('A Test Recipe', 'This is simply a test', 'https://www.recipetineats.com/wp-content/uploads/2023/09/Homemade-pork-sausage-patties_2.jpg' )
+];
+
+constructor() { }
+
+ngOnInit() {
+
+}
 
 }
