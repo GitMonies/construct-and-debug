b2bbd87e9eb31ed81311d60837a4b7467aa72516
GitMonies <jarod.smelosky@yahoo.com>
2023-10-10 20:40:33 -0500

    Outputting a List of Recipes with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 recipes: Recipe[] = [
-  new Recipe('A Test Recipe', 'This is simply a test', 'https://www.recipetineats.com/wp-content/uploads/2023/09/Homemade-pork-sausage-patties_2.jpg' )
+  new Recipe('A Test Recipe', 'This is simply a test', 'https://www.recipetineats.com/wp-content/uploads/2023/09/Homemade-pork-sausage-patties_2.jpg' ),
+new Recipe('A Test Recipe', 'This is simply a test', 'https://www.recipetineats.com/wp-content/uploads/2023/09/Homemade-pork-sausage-patties_2.jpg' )
 ];
 
 constructor() { }
